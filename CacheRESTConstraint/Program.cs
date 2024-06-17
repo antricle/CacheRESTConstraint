7c7c878b4273021ffe2cf588b1d339e16a90e35f
antricle <antricle@outlook.com>
2024-06-17 12:07:52 +0100

    Update Program.cs

--- CacheRESTConstraint/Program.cs
+++ CacheRESTConstraint/Program.cs
@@ -3,11 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddResponseCaching();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseResponseCaching();
 app.UseAuthorization();
 
 app.MapControllers();
